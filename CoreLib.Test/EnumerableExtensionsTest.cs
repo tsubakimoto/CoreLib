06cebdfcfad0a5bc8fd4ef5d4126d10145086257
tsubakimoto <tkmbmbr@gmail.com>
2015-05-02 08:41:02 +0900

    add ForEach method and test

---
+++ CoreLib.Test/EnumerableExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tsubaki.CoreLib;
+
+namespace Tsubaki.CoreLib.Test
+{
+    [TestClass]
+    public class EnumerableExtensionsTest
+    {
+        [TestMethod]
+        public void ForEach()
+        {
+            var actual = new List<int>();
+            var expected = new List<int> { 1, 2, 3, 4, 5 };
+            Enumerable.Range(1, 5).ForEach(i => actual.Add(i));
+            for (var i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ CoreLib/EnumerableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsubaki.CoreLib
+{
+    public static class EnumerableExtensions
+    {
+        public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
+        {
+            foreach (var item in source) { action(item); }
+        }
+    }
+}
