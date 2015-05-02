bf24829109cad59cc03c5aba3db18dcd3293faec
tsubakimoto <tkmbmbr@gmail.com>
2015-05-02 12:15:33 +0900

    add Merge method and test

--- CoreLib.Test/EnumerableExtensionsTest.cs
+++ CoreLib.Test/EnumerableExtensionsTest.cs
@@ -10,11 +10,22 @@ namespace Tsubaki.CoreLib.Test
     public class EnumerableExtensionsTest
     {
         [TestMethod]
-        public void ForEach()
+        public void ForEachTest()
         {
             var actual = new List<int>();
             var expected = new List<int> { 1, 2, 3, 4, 5 };
             Enumerable.Range(1, 5).ForEach(i => actual.Add(i));
+            for (var i = 0; i < 5; i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+
+        [TestMethod]
+        public void MergeTest()
+        {
+            var expected = new List<int> { 1, 2, 3, 4, 5 };
+            var actual = Enumerable.Range(1, 3).Merge(Enumerable.Range(4, 2)).ToList();
             for (var i = 0; i < 5; i++)
             {
                 Assert.AreEqual(expected[i], actual[i]);
--- CoreLib/EnumerableExtensions.cs
+++ CoreLib/EnumerableExtensions.cs
@@ -12,5 +12,12 @@ namespace Tsubaki.CoreLib
         {
             foreach (var item in source) { action(item); }
         }
+
+        public static IEnumerable<TSource> Merge<TSource>(this IEnumerable<TSource> source1, IEnumerable<TSource> source2)
+        {
+            var result = new List<TSource>(source1);
+            result.AddRange(source2);
+            return result;
+        }
     }
 }
